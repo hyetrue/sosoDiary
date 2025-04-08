f0e907667f8b20cd0e5412c5c4aa2fc63c4e3e28
hyejin <hyejin6829@gmail.com>
2025-04-08 17:31:56 +0900

    홈 수정

--- src/components/LatestItem.js
+++ src/components/LatestItem.js
@@ -1,7 +1,8 @@
-import { useNavigate } from "react-router-dom";
-import ing from "./../pages/images/ing.png";
+import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import ing from './../pages/images/ing.png';
 
-const LatestItem = ({ id, files, date }) => {
+const LatestItem = ({ id, files, date, content }) => {
   const navigate = useNavigate();
 
   const srtDate = new Date(parseInt(date)).toLocaleDateString();
@@ -11,17 +12,26 @@ const LatestItem = ({ id, files, date }) => {
   };
 
   return (
-    <div className="recent-box">
-      <div className="list_item" onClick={goDetail}>
-        <img className="list_img" src={ing} />
-        {/* <img
-          className="list_img"
-          src={files ? URL.createObjectURL(files) : "null"}
-        /> */}
+    <div className='recent-box'>
+      <div className='list_item' onClick={goDetail}>
+        <img
+          className='list_img'
+          src={
+            files && files[0]
+              ? files[0]
+              : process.env.PUBLIC_URL + '/assets/default-diary.png'
+          }
+          alt='데이터 이미지'
+        />
       </div>
-      <div className="list_text">{srtDate}</div>
+      <div className='latest-info'>
+        <div className='latest-date'>{srtDate}</div>
+        <div className='latest-content'>
+          {content && content.slice(0, 30)}...
+        </div>
+      </div>
     </div>
   );
 };
 
-export default LatestItem;
+export default React.memo(LatestItem);
